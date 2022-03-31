2ddf67a5acdf2ebea0a7f27d331ffa844f41fdee
kvit <kutukov@list.ru>
2022-03-31 21:05:42 +0300

    Initial commit

---
+++ Fish.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kvit on 31.03.2022.
+//
+
+#include "Fish.h"
---
+++ Fish.h
@@ -0,0 +1,14 @@
+//
+// Created by kvit on 31.03.2022.
+//
+
+#ifndef HOMEWORK33_5_2_FISH_H
+#define HOMEWORK33_5_2_FISH_H
+
+
+class Fish {
+
+};
+
+
+#endif //HOMEWORK33_5_2_FISH_H
